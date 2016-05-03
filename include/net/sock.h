1cef3b87aa165f099761efe74e4350052243c43f
Eric Dumazet <edumazet@google.com>
2016-05-03 16:56:03 -0700

    tcp: fix lockdep splat in tcp_snd_una_update()
    tcp_snd_una_update() and tcp_rcv_nxt_update() call
    u64_stats_update_begin() either from process context or BH handler.
    
    This triggers a lockdep splat on 32bit & SMP builds.
    
    We could add u64_stats_update_begin_bh() variant but this would
    slow down 32bit builds with useless local_disable_bh() and
    local_enable_bh() pairs, since we own the socket lock at this point.
    
    I add sock_owned_by_me() helper to have proper lockdep support
    even on 64bit builds, and new u64_stats_update_begin_raw()
    and u64_stats_update_end_raw methods.
    
    Fixes: c10d9310edf5 ("tcp: do not assume TCP code is non preemptible")
    Reported-by: Fabio Estevam <festevam@gmail.com>
    Diagnosed-by: Francois Romieu <romieu@fr.zoreil.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Tested-by: Fabio Estevam <fabio.estevam@nxp.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1468,7 +1468,18 @@ static inline void sk_wmem_free_skb(struct sock *sk, struct sk_buff *skb)
  * Since ~2.3.5 it is also exclusive sleep lock serializing
  * accesses from user process context.
  */
-#define sock_owned_by_user(sk)	((sk)->sk_lock.owned)
+static inline void sock_owned_by_me(const struct sock *sk)
+{
+#ifdef CONFIG_LOCKDEP
+	WARN_ON_ONCE(!lockdep_sock_is_held(sk) && debug_locks);
+#endif
+}
+
+static inline bool sock_owned_by_user(const struct sock *sk)
+{
+	sock_owned_by_me(sk);
+	return sk->sk_lock.owned;
+}
 
 static inline void sock_release_ownership(struct sock *sk)
 {
