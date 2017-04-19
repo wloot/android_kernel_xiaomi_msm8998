796091c728e24eb1d174822bb8085adc8d5b530f
Devin Kim <dojip.kim@lge.com>
2017-04-19 11:56:43 -0700

    scsi: ufs: Fix scale up issue
    The scale up won't work after commit d0960c5 "scsi: ufs: scale up
    the gear in 2 steps" if the device doesn't need
    UFS_DEVICE_QUIRK_HS_G1_TO_HS_G3_SWITCH quirk.
    
    This scale up the gear with original request in that case.
    
    Bug: 37494415
    Change-Id: I0b3fc4d30ce3e63722e2c14c49f210a606b8068b
    Signed-off-by: Devin Kim <dojip.kim@lge.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -9519,11 +9519,7 @@ static int ufshcd_scale_gear(struct ufs_hba *hba, bool scale_up)
 			/* scale up to G3 now */
 			new_pwr_info.gear_tx = UFS_HS_G3;
 			new_pwr_info.gear_rx = UFS_HS_G3;
-			/* now, fall through to set the HS-G3 */
 		}
-		ret = ufshcd_change_power_mode(hba, &new_pwr_info);
-		if (ret)
-			goto out;
 	} else {
 		memcpy(&new_pwr_info, &hba->pwr_info,
 		       sizeof(struct ufs_pa_layer_attr));
@@ -9543,8 +9539,8 @@ static int ufshcd_scale_gear(struct ufs_hba *hba, bool scale_up)
 				new_pwr_info.pwr_rx = FASTAUTO_MODE;
 			}
 		}
-		ret = ufshcd_change_power_mode(hba, &new_pwr_info);
 	}
+	ret = ufshcd_change_power_mode(hba, &new_pwr_info);
 
 out:
 	if (ret)
