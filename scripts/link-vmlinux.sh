2bae99e876286a6d9f31501cc5f2547bd99247bc
Sami Tolvanen <samitolvanen@google.com>
2017-06-16 12:52:57 -0700

    FROMLIST: BACKPORT: kbuild: fix dynamic ftrace with clang LTO
    With CONFIG_LTO_CLANG enabled, LLVM IR won't be compiled into object
    files until modpost_link. This change postpones calls to recordmcount
    until after this step.
    
    In order to exclude ftrace_process_locs from inspection, we add a new
    code section .text..ftrace, which we tell recordmcount to ignore, and
    a __norecordmcount attribute for moving functions to this section.
    
    Bug: 62093296
    Bug: 67506682
    Change-Id: Iba2c053968206acf533fadab1eb34a743b5088ee
    (am from https://patchwork.kernel.org/patch/10060327/)
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -115,6 +115,19 @@ modpost_link()
 	${LD} ${LDFLAGS} -r -o ${1} $(modversions) ${objects}
 }
 
+# If CONFIG_LTO_CLANG is selected, we postpone running recordmcount until
+# we have compiled LLVM IR to an object file.
+recordmcount()
+{
+	if [ -z "${CONFIG_LTO_CLANG}" ]; then
+		return
+	fi
+
+	if [ -n "${CONFIG_FTRACE_MCOUNT_RECORD}" ]; then
+		scripts/recordmcount ${RECORDMCOUNT_FLAGS} $*
+	fi
+}
+
 # Link of vmlinux
 # ${1} - optional extra .o files
 # ${2} - output file
@@ -273,6 +286,11 @@ modpost_link vmlinux.o
 
 # modpost vmlinux.o to check for section mismatches
 ${MAKE} -f "${srctree}/scripts/Makefile.modpost" vmlinux.o
+
+if [ -n "${CONFIG_LTO_CLANG}" ]; then
+	# Call recordmcount if needed
+	recordmcount vmlinux.o
+fi
 
 kallsymso=""
 kallsyms_vmlinux=""
