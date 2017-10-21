b0af0b2ea333e7aa3bf0ab0b351e2ffbcfc28949
Vincent Guittot <vincent.guittot@linaro.org>
2017-10-21 14:19:26 +0100

    FROMLIST: sched/rt: add utilization tracking
    schedutil governor relies on cfs_rq's util_avg to choose the OPP when cfs
    tasks are running. When the CPU is overloaded by cfs and rt tasks, cfs tasks
    are preempted by rt tasks and in this case util_avg reflects the remaining
    capacity that is used by cfs tasks but not what cfs tasks want to use. In such
    case, schedutil can select a lower OPP when cfs task runs whereas the CPU is
    overloaded. In order to have a more accurate view of the utilization of the
    CPU, we track the utilization that is used by RT tasks.
    DL tasks are not taken into account as they have their own utilization
    tracking mecanism.
    
    We don't use rt_avg which doesn't have the same dynamic as PELT and which
    can include IRQ time.
    
    Change-Id: Icf36a67ea516b1ab21d9443a70e4b253f248550f
    Signed-off-by: Vincent Guittot <vincent.guittot@linaro.org>
    [backported from list: http://marc.info/?l=linux-kernel&m=149786230529220]
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    [backported from android-4.14]
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2830,7 +2830,8 @@ accumulate_sum(u64 delta, int cpu, struct sched_avg *sa,
  */
 static __always_inline int
 ___update_load_avg(u64 now, int cpu, struct sched_avg *sa,
-		  unsigned long weight, int running, struct cfs_rq *cfs_rq)
+		  unsigned long weight, int running, struct cfs_rq *cfs_rq,
+		  struct rt_rq *rt_rq)
 {
 	u64 delta;
 
@@ -2880,7 +2881,7 @@ ___update_load_avg(u64 now, int cpu, struct sched_avg *sa,
 static int
 __update_load_avg_blocked_se(u64 now, int cpu, struct sched_entity *se)
 {
-	return ___update_load_avg(now, cpu, &se->avg, 0, 0, NULL);
+	return ___update_load_avg(now, cpu, &se->avg, 0, 0, NULL, NULL);
 }
 
 static int
@@ -2888,7 +2889,7 @@ __update_load_avg_se(u64 now, int cpu, struct cfs_rq *cfs_rq, struct sched_entit
 {
 	return ___update_load_avg(now, cpu, &se->avg,
 				  se->on_rq * scale_load_down(se->load.weight),
-				  cfs_rq->curr == se, NULL);
+				  cfs_rq->curr == se, NULL, NULL);
 }
 
 static int
@@ -2896,7 +2897,7 @@ __update_load_avg_cfs_rq(u64 now, int cpu, struct cfs_rq *cfs_rq)
 {
 	return ___update_load_avg(now, cpu, &cfs_rq->avg,
 			scale_load_down(cfs_rq->load.weight),
-			cfs_rq->curr != NULL, cfs_rq);
+			cfs_rq->curr != NULL, cfs_rq, NULL);
 }
 
 /*
@@ -3232,6 +3233,15 @@ update_cfs_rq_load_avg(u64 now, struct cfs_rq *cfs_rq, bool update_freq)
 	return decayed || removed;
 }
 
+int update_rt_rq_load_avg(u64 now, int cpu, struct rt_rq *rt_rq, int running)
+{
+	int ret;
+
+	ret = ___update_load_avg(now, cpu, &rt_rq->avg, 0, running, NULL, rt_rq);
+
+	return ret;
+}
+
 /*
  * Optional action to be done while updating the load average
  */
@@ -3425,6 +3435,11 @@ static int idle_balance(struct rq *this_rq);
 
 static inline int
 update_cfs_rq_load_avg(u64 now, struct cfs_rq *cfs_rq, bool update_freq)
+{
+	return 0;
+}
+
+int update_rt_rq_load_avg(u64 now, int cpu, struct rt_rq *rt_rq, int running)
 {
 	return 0;
 }
@@ -7877,6 +7892,7 @@ static void update_blocked_averages(int cpu)
 		if (cfs_rq->tg->se[cpu])
 			update_load_avg(cfs_rq->tg->se[cpu], 0);
 	}
+	update_rt_rq_load_avg(rq_clock_task(rq), cpu, &rq->rt, 0);
 	raw_spin_unlock_irqrestore(&rq->lock, flags);
 }
 
@@ -7936,6 +7952,7 @@ static inline void update_blocked_averages(int cpu)
 	raw_spin_lock_irqsave(&rq->lock, flags);
 	update_rq_clock(rq);
 	update_cfs_rq_load_avg(cfs_rq_clock_task(cfs_rq), cfs_rq, true);
+	update_rt_rq_load_avg(rq_clock_task(rq), cpu, &rq->rt, 0);
 	raw_spin_unlock_irqrestore(&rq->lock, flags);
 }
 
