36b4cdf049db0898de85681d3698a25f44ee18a8
Sami Tolvanen <samitolvanen@google.com>
2017-11-28 08:48:49 -0800

    ANDROID: kbuild: add support for clang LTO
    This change adds the configuration option CONFIG_LTO_CLANG, and
    build system support for clang's Link Time Optimization (LTO). In
    preparation for LTO support for other compilers, potentially common
    parts of the changes are gated behind CONFIG_LTO instead.
    
    With -flto, instead of object files, clang produces LLVM bitcode,
    which is compiled into a native object at link time, allowing the
    final binary to be optimized globally. For more details, see:
    
      https://llvm.org/docs/LinkTimeOptimization.html
    
    While the kernel normally uses GNU ld for linking, LLVM supports LTO
    only with lld or GNU gold linkers. This patch set assumes lld will
    be used.
    
    Bug: 62093296
    Bug: 67506682
    Bug: 133186739
    Change-Id: Ibcd9fc7ec501b4f30b43b4877897615645f8655f
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>
    [kenny3fcb: Add LLD linking support from commit: https://github.com/kdrag0n/proton_bluecross/commit/aba52591410b75e6d21df015d1c3ebba051677e3,
    substitute CC_IS_CLANG with clang-ifversion check taken from kernel 4.9 version of this commit,
    add ld-name macro from https://github.com/aosp-mirror/kernel_common/commit/552777bdc1d775bee1ffde43c23ab0b2c4501f35, replace gold with lld referring the above kdrag0n's commit,
    substitute LD_IS_LLD with ld-name check,
    add back lto-flags check from kernel 4.9 version of this commit]
    Signed-off-by: Subhajeet Muhuri <kenny3fcb@gmail.com>
    [wight554: add ThinLTO support from upstream version of commit,
    check for clang 8.0 instead of 5.0 for LD=ld.lld compatibility]
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -59,6 +59,38 @@ archive_builtin()
 	${AR} rcsTP${KBUILD_ARFLAGS} built-in.a			\
 				${KBUILD_VMLINUX_INIT}		\
 				${KBUILD_VMLINUX_MAIN}
+
+	# rebuild with llvm-ar to update the symbol table
+	if [ -n "${CONFIG_LTO_CLANG}" ]; then
+		mv -f built-in.a built-in.a.tmp
+		${LLVM_AR} rcsT${KBUILD_ARFLAGS} built-in.a $(${AR} t built-in.a.tmp)
+		rm -f built-in.a.tmp
+	fi
+}
+
+# If CONFIG_LTO_CLANG is selected, collect generated symbol versions into
+# .tmp_symversions
+modversions()
+{
+	if [ -z "${CONFIG_LTO_CLANG}" ]; then
+		return
+	fi
+
+	if [ -z "${CONFIG_MODVERSIONS}" ]; then
+		return
+	fi
+
+	rm -f .tmp_symversions
+
+	for a in built-in.a ${KBUILD_VMLINUX_LIBS}; do
+		for o in $(${AR} t $a); do
+			if [ -f ${o}.symversions ]; then
+				cat ${o}.symversions >> .tmp_symversions
+			fi
+		done
+	done
+
+	echo "-T .tmp_symversions"
 }
 
 # Link of vmlinux.o used for section mismatch analysis
@@ -74,7 +106,13 @@ modpost_link()
 		${KBUILD_VMLINUX_LIBS}				\
 		--end-group"
 
-	${LD} ${LDFLAGS} -r -o ${1} ${objects}
+	if [ -n "${CONFIG_LTO_CLANG}" ]; then
+		# This might take a while, so indicate that we're doing
+		# an LTO link
+		info LTO vmlinux.o
+	fi
+
+	${LD} ${LDFLAGS} -r -o ${1} $(modversions) ${objects}
 }
 
 # Link of vmlinux
@@ -86,13 +124,20 @@ vmlinux_link()
 	local objects
 
 	if [ "${SRCARCH}" != "um" ]; then
-		objects="--whole-archive			\
-			built-in.a				\
-			--no-whole-archive			\
-			--start-group				\
-			${KBUILD_VMLINUX_LIBS}			\
-			--end-group				\
-			${1}"
+		if [ -z "${CONFIG_LTO_CLANG}" ]; then
+			objects="--whole-archive		\
+				built-in.a			\
+				--no-whole-archive		\
+				--start-group			\
+				${KBUILD_VMLINUX_LIBS}		\
+				--end-group			\
+				${1}"
+		else
+			objects="--start-group			\
+				vmlinux.o			\
+				--end-group			\
+				${1}"
+		fi
 
 		${LD} ${LDFLAGS} ${LDFLAGS_vmlinux} -o ${2}	\
 			-T ${lds} ${objects}
@@ -112,7 +157,6 @@ vmlinux_link()
 		rm -f linux
 	fi
 }
-
 
 # Create ${2} .o file with all symbols from the ${1} object file
 kallsyms()
@@ -162,6 +206,7 @@ cleanup()
 	rm -f .old_version
 	rm -f .tmp_System.map
 	rm -f .tmp_kallsyms*
+	rm -f .tmp_symversions
 	rm -f .tmp_version
 	rm -f .tmp_vmlinux*
 	rm -f built-in.a
@@ -224,7 +269,6 @@ ${MAKE} -f "${srctree}/scripts/Makefile.build" obj=init
 archive_builtin
 
 #link vmlinux.o
-info LD vmlinux.o
 modpost_link vmlinux.o
 
 # modpost vmlinux.o to check for section mismatches
