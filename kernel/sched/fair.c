cd429919d2f003d17bb6f469a46bf97ec9f61c6d
Ionela Voinescu <ionela.voinescu@arm.com>
2017-12-07 19:43:46 +0000

    sched/fair: introduce minimum capacity capping sched feature
    We have the ability to track minimum capacity forced onto a CPU by
    userspace or external actors. This is provided though a minimum
    frequency scale factor exposed by arch_scale_min_freq_capacity.
    
    The use of this information is enabled through the MIN_CAPACITY_CAPPING
    feature. If not enabled, the minimum frequency scale factor will
    remain 0 and it will not impact energy estimation or scheduling
    decisions.
    
    Change-Id: Ibc61f2bf4fddf186695b72b262e602a6e8bfde37
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6441,6 +6441,19 @@ unsigned long capacity_curr_of(int cpu)
 	       >> SCHED_CAPACITY_SHIFT;
 }
 
+/*
+ * Returns the current capacity of cpu after applying both
+ * cpu and min freq scaling.
+ */
+unsigned long capacity_min_of(int cpu)
+{
+	if (!sched_feat(MIN_CAPACITY_CAPPING))
+		return 0;
+	return arch_scale_cpu_capacity(NULL, cpu) *
+	       arch_scale_min_freq_capacity(NULL, cpu)
+	       >> SCHED_CAPACITY_SHIFT;
+}
+
 struct energy_env {
 	struct sched_group	*sg_top;
 	struct sched_group	*sg_cap;
