85692ce53b5179cc557de7999ed4e0d1f272c090
Nathan Chancellor <natechancellor@gmail.com>
2018-04-25 18:54:22 -0700

    fpc1268: remove duplicate const specifier
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Change-Id: I967edea632a7d84ab7bfbff3ad6a80574e37d0f8

--- drivers/input/fingerprint/fpc1268_tee/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc1268_tee/fpc1020_tee.c
@@ -68,7 +68,7 @@ struct vreg_config {
 	int ua_load;
 };
 
-static const struct vreg_config const vreg_conf[] = {
+static const struct vreg_config vreg_conf[] = {
 	{ "vdd_ana", 1800000UL, 1800000UL, 6000, },
 };
 
