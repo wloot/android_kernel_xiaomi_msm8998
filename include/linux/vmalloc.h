328dacde93107895d5170003c81485d63664844d
Kees Cook <keescook@chromium.org>
2018-05-08 12:55:26 -0700

    BACKPORT: mm: Use overflow helpers in kvmalloc()
    Instead of open-coded multiplication and bounds checking, use the new
    overflow helper. Additionally prepare for vmalloc() users to add
    array_size()-family helpers in the future.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    (cherry picked from commit 3b3b1a29eb89ba93f91213cbebb332a2ac31fa8b)
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- include/linux/vmalloc.h
+++ include/linux/vmalloc.h
@@ -7,6 +7,7 @@
 #include <linux/llist.h>
 #include <asm/page.h>		/* pgprot_t */
 #include <linux/rbtree.h>
+#include <linux/overflow.h>
 
 struct vm_area_struct;		/* vma defining user mapping in mm_types.h */
 
