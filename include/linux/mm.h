d73098037659e19d9ee90a227ae999548336ec0a
Kees Cook <keescook@chromium.org>
2018-06-11 14:35:55 -0700

    UPSTREAM: mm: Introduce kvcalloc()
    The kv*alloc()-family was missing kvcalloc(). Adding this allows for
    2-argument multiplication conversions of kvzalloc(a * b, ...) into
    kvcalloc(a, b, ...).
    
    Signed-off-by: Kees Cook <keescook@chromium.org>
    (cherry picked from commit 1c542f38ab8d30d9c852a16d49ac5a15267bbf1f)
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -438,6 +438,11 @@ static inline void *kvmalloc_array(size_t n, size_t size, gfp_t flags)
 	return kvmalloc(bytes, flags);
 }
 
+static inline void *kvcalloc(size_t n, size_t size, gfp_t flags)
+{
+	return kvmalloc_array(n, size, flags | __GFP_ZERO);
+}
+
 extern void kvfree(const void *addr);
 
 static inline void compound_lock(struct page *page)
