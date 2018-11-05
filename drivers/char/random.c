42e0e06003b1c1125b6fbf10157d50d2fbd7c667
wloot <wlootlxt123@gmail.com>
2018-11-06 04:41:07 +0800

    Revert "Remove unused function to fix compilation"
    This reverts commit 27ddc8bb95080b16bfa007c1df8c830a2ae758c2.

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1457,6 +1457,12 @@ _random_read(int nonblock, char __user *buf, size_t nbytes)
 }
 
 static ssize_t
+random_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
+{
+	return _random_read(file->f_flags & O_NONBLOCK, buf, nbytes);
+}
+
+static ssize_t
 urandom_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 {
 	static int maxwarn = 10;
