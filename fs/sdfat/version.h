edfdeab73d72e436c629236b829c56f83dc2b2af
derfelot <5124143+derfelot@users.noreply.github.com>
2019-01-06 00:33:53 +0100

    fs: sdfat: Update to version 2.1.8
    Taken from SM-G965F_PP_Opensource kernel (G965FXXU2CRLI)

--- fs/sdfat/version.h
+++ fs/sdfat/version.h
@@ -22,4 +22,4 @@
 /*  PURPOSE : sdFAT File Manager                                        */
 /*                                                                      */
 /************************************************************************/
-#define SDFAT_VERSION	"2.0.8"
+#define SDFAT_VERSION	"2.1.8"
