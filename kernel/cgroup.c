2e54becbaacf099c2c842398d81e6155c9c3f547
wloot <wlootlxt123@gmail.com>
2019-03-08 10:30:50 +0800

    kernel: Remove top app boost

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -60,9 +60,6 @@
 #include <linux/cpuset.h>
 #include <linux/atomic.h>
 #include <linux/freezer.h>
-#include <linux/binfmts.h>
-#include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 
 /*
  * pidlists linger the following amount before being destroyed.  The goal
@@ -2778,14 +2775,6 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 	ret = cgroup_procs_write_permission(tsk, cgrp, of);
 	if (!ret)
 		ret = cgroup_attach_task(cgrp, tsk, threadgroup);
-
-	/* This covers boosting for app launches and app transitions */
-	if (!ret && !threadgroup &&
-		!memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
-		is_zygote_pid(tsk->parent->pid)) {
-		cpu_input_boost_kick_max(500);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500);
-	}
 
 	put_task_struct(tsk);
 	goto out_unlock_threadgroup;
