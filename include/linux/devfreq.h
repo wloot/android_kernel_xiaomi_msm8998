989d23515c5f4b7641c5d781d7d38adc5b73e202
wloot <wlootlxt123@gmail.com>
2019-03-10 18:09:45 +0800

    treewide: Revert all sultan's boost drives

--- include/linux/devfreq.h
+++ include/linux/devfreq.h
@@ -175,8 +175,6 @@ struct devfreq {
 
 	unsigned long min_freq;
 	unsigned long max_freq;
-	bool is_boost_device;
-	bool max_boost;
 	bool stop_polling;
 
 	/* information for device frequency transition */
@@ -252,9 +250,6 @@ struct devfreq_simple_ondemand_data {
 };
 #endif
 
-/* Caution: devfreq->lock must be locked before calling update_devfreq */
-extern int update_devfreq(struct devfreq *devfreq);
-
 #else /* !CONFIG_PM_DEVFREQ */
 static inline struct devfreq *devfreq_add_device(struct device *dev,
 					  struct devfreq_dev_profile *profile,
@@ -322,11 +317,6 @@ static inline void devm_devfreq_unregister_opp_notifier(struct device *dev,
 }
 
 static inline int devfreq_update_stats(struct devfreq *df)
-{
-	return -EINVAL;
-}
-
-static inline int update_devfreq(struct devfreq *devfreq)
 {
 	return -EINVAL;
 }
