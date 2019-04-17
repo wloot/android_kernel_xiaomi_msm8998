95a7b44e609e84306f73965150511a27f91caa45
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-17 11:13:45 -0700

    synaptics_dsx_force: Mark IRQs as performance critical
    Touch latency is pretty important; let's service the touch IRQs and
    associated kthreads on fast CPUs to improve touch responsiveness.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [wight554: adapted wahoo ts change for synaptics_dsx_force]
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>

--- drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_core.c
+++ drivers/input/touchscreen/synaptics_dsx_force/synaptics_dsx_core.c
@@ -2071,7 +2071,7 @@ static int synaptics_rmi4_irq_enable(struct synaptics_rmi4_data *rmi4_data,
 		synaptics_rmi4_sensor_report(rmi4_data, false);
 
 		retval = request_threaded_irq(rmi4_data->irq, NULL,
-				synaptics_rmi4_irq, bdata->irq_flags,
+				synaptics_rmi4_irq, bdata->irq_flags | IRQF_PERF_CRITICAL,
 				PLATFORM_DRIVER_NAME, rmi4_data);
 		if (retval < 0) {
 			dev_err(rmi4_data->pdev->dev.parent,
