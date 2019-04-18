c7e0161f3f659ab296d02a9ee1c2f616ec4e1d16
wloot <wlootlxt123@gmail.com>
2019-04-18 13:49:50 +0800

    Revert "power: msm-core: Fix mutex not getting unlocked in error path"
    This reverts commit f7f96d124265b72181b03a3ee4fddce1f4855ccf.

--- drivers/power/qcom/msm-core.c
+++ drivers/power/qcom/msm-core.c
@@ -536,7 +536,7 @@ static long msm_core_ioctl(struct file *file, unsigned int cmd,
 				node->sp->voltage,
 				sizeof(uint32_t) * node->sp->num_of_freqs);
 		if (ret)
-			goto unlock;
+			break;
 		for (i = 0; i < node->sp->num_of_freqs; i++) {
 			ret = copy_to_user((void __user *)&argp->freq[i],
 					&node->sp->table[i].frequency,
