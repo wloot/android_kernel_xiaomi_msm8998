a94db33fffc6de5cde3fe8af25e7489b49014547
wloot <wlootlxt123@gmail.com>
2019-05-02 20:50:01 +0800

    Revert "qpnp-wled: disable ovp irq after handling"
    This reverts commit 6a9d2e9ab78f2f6d31d128d8fe8d9a0bf62294a2.

--- drivers/leds/leds-qpnp-wled.c
+++ drivers/leds/leds-qpnp-wled.c
@@ -1518,7 +1518,7 @@ static irqreturn_t qpnp_wled_ovp_irq_handler(int irq, void *_wled)
 			QPNP_WLED_FAULT_STATUS(wled->ctrl_base), &fault_sts);
 	if (rc < 0) {
 		pr_err("Error in reading WLED_FAULT_STATUS rc=%d\n", rc);
-		goto end;
+		return IRQ_HANDLED;
 	}
 
 	if (fault_sts & (QPNP_WLED_OVP_FAULT_BIT | QPNP_WLED_ILIM_FAULT_BIT))
@@ -1552,9 +1552,6 @@ static irqreturn_t qpnp_wled_ovp_irq_handler(int irq, void *_wled)
 		}
 	}
 
-end:
-	disable_irq_nosync(wled->ovp_irq);
-	wled->ovp_irq_disabled = true;
 	return IRQ_HANDLED;
 }
 
