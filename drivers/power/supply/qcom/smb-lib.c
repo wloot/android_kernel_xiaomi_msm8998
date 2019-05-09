88f4e3534499fe49d72c162effc8a17e647e8fab
wloot <wlootlxt123@gmail.com>
2019-05-09 18:40:10 +0800

    smb-lib: Force usb 900MA charging if 500MA was inputted

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -822,6 +822,9 @@ static int set_sdp_current(struct smb_charger *chg, int icl_ua)
 	u8 icl_options;
 	const struct apsd_result *apsd_result = smblib_get_apsd_result(chg);
 
+	if (icl_ua == USBIN_500MA)
+		icl_ua = USBIN_900MA;
+
 	/* power source is SDP */
 	switch (icl_ua) {
 	case USBIN_100MA:
