c0707cd265746284aafbe348b2e2989e5b0ae19e
wloot <wlootlxt123@gmail.com>
2019-06-29 23:34:40 +0800

    base: make device_{online,offline} no op

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -1524,6 +1524,7 @@ int __init devices_init(void)
 	return -ENOMEM;
 }
 
+#if 0
 static int device_check_offline(struct device *dev, void *not_used)
 {
 	int ret;
@@ -1534,6 +1535,7 @@ static int device_check_offline(struct device *dev, void *not_used)
 
 	return device_supports_offline(dev) && !dev->offline ? -EBUSY : 0;
 }
+#endif
 
 /**
  * device_offline - Prepare the device for hot-removal.
@@ -1548,8 +1550,9 @@ static int device_check_offline(struct device *dev, void *not_used)
  */
 int device_offline(struct device *dev)
 {
-	int ret;
+	int ret = 0;
 
+#if 0
 	if (dev->offline_disabled)
 		return -EPERM;
 
@@ -1570,6 +1573,7 @@ int device_offline(struct device *dev)
 		}
 	}
 	device_unlock(dev);
+#endif
 
 	return ret;
 }
@@ -1588,6 +1592,7 @@ int device_online(struct device *dev)
 {
 	int ret = 0;
 
+#if 0
 	device_lock(dev);
 	if (device_supports_offline(dev)) {
 		if (dev->offline) {
@@ -1601,6 +1606,7 @@ int device_online(struct device *dev)
 		}
 	}
 	device_unlock(dev);
+#endif
 
 	return ret;
 }
