0e8c8b966e91a763c490b48db13e2666c68202de
wloot <wlootlxt123@gmail.com>
2019-07-01 12:50:42 +0800

    Revert "ion: Reserve caches for sg allocations"
    This reverts commit 9ee8df36541e6e9ed15ee30970d08395a2d92eaf.

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -124,8 +124,6 @@ struct ion_handle {
 };
 
 static struct ion_device *ion_dev;
-static struct kmem_cache *ion_page_pool;
-static struct kmem_cache *ion_sg_table_pool;
 
 bool ion_buffer_fault_user_mappings(struct ion_buffer *buffer)
 {
@@ -1024,34 +1022,6 @@ struct sg_table *ion_sg_table(struct ion_client *client,
 }
 EXPORT_SYMBOL(ion_sg_table);
 
-static struct scatterlist *ion_sg_alloc(unsigned int nents, gfp_t gfp_mask)
-{
-	if (nents == SG_MAX_SINGLE_ALLOC)
-		return kmem_cache_alloc(ion_page_pool, gfp_mask);
-
-	return kmalloc(nents * sizeof(struct scatterlist), gfp_mask);
-}
-
-static void ion_sg_free(struct scatterlist *sg, unsigned int nents)
-{
-	if (nents == SG_MAX_SINGLE_ALLOC)
-		kmem_cache_free(ion_page_pool, sg);
-	else
-		kfree(sg);
-}
-
-static int ion_sg_alloc_table(struct sg_table *table, unsigned int nents,
-			      gfp_t gfp_mask)
-{
-	return __sg_alloc_table(table, nents, SG_MAX_SINGLE_ALLOC, NULL,
-				gfp_mask, ion_sg_alloc);
-}
-
-static void ion_sg_free_table(struct sg_table *table)
-{
-	__sg_free_table(table, SG_MAX_SINGLE_ALLOC, false, ion_sg_free);
-}
-
 struct sg_table *ion_create_chunked_sg_table(phys_addr_t buffer_base,
 					size_t chunk_size, size_t total_size)
 {
@@ -1059,14 +1029,14 @@ struct sg_table *ion_create_chunked_sg_table(phys_addr_t buffer_base,
 	int i, n_chunks, ret;
 	struct scatterlist *sg;
 
-	table = kmem_cache_alloc(ion_sg_table_pool, GFP_KERNEL);
+	table = kzalloc(sizeof(struct sg_table), GFP_KERNEL);
 	if (!table)
 		return ERR_PTR(-ENOMEM);
 
 	n_chunks = DIV_ROUND_UP(total_size, chunk_size);
 	pr_debug("creating sg_table with %d chunks\n", n_chunks);
 
-	ret = ion_sg_alloc_table(table, n_chunks, GFP_KERNEL);
+	ret = sg_alloc_table(table, n_chunks, GFP_KERNEL);
 	if (ret)
 		goto err0;
 
@@ -1078,7 +1048,7 @@ struct sg_table *ion_create_chunked_sg_table(phys_addr_t buffer_base,
 
 	return table;
 err0:
-	kmem_cache_free(ion_sg_table_pool, table);
+	kfree(table);
 	return ERR_PTR(ret);
 }
 
@@ -1088,19 +1058,19 @@ static struct sg_table *ion_dupe_sg_table(struct sg_table *orig_table)
 	struct scatterlist *sg, *sg_orig;
 	struct sg_table *table;
 
-	table = kmem_cache_alloc(ion_sg_table_pool, GFP_KERNEL);
+	table = kzalloc(sizeof(struct sg_table), GFP_KERNEL);
 	if (!table)
 		return NULL;
 
-	ret = ion_sg_alloc_table(table, orig_table->nents, GFP_KERNEL);
+	ret = sg_alloc_table(table, orig_table->nents, GFP_KERNEL);
 	if (ret) {
-		kmem_cache_free(ion_sg_table_pool, table);
+		kfree(table);
 		return NULL;
 	}
 
 	sg_orig = orig_table->sgl;
 	for_each_sg(table->sgl, sg, table->nents, i) {
-		*sg = *sg_orig;
+		memcpy(sg, sg_orig, sizeof(*sg));
 		sg_orig = sg_next(sg_orig);
 	}
 	return table;
@@ -1129,8 +1099,8 @@ static void ion_unmap_dma_buf(struct dma_buf_attachment *attachment,
 			      struct sg_table *table,
 			      enum dma_data_direction direction)
 {
-	ion_sg_free_table(table);
-	kmem_cache_free(ion_sg_table_pool, table);
+	sg_free_table(table);
+	kfree(table);
 }
 
 void ion_pages_sync_for_device(struct device *dev, struct page *page,
@@ -2014,20 +1984,6 @@ struct ion_device *ion_device_create(long (*custom_ioctl)
 	if (!idev)
 		return ERR_PTR(-ENOMEM);
 
-	ion_sg_table_pool = KMEM_CACHE(sg_table, SLAB_HWCACHE_ALIGN);
-	if (!ion_sg_table_pool) {
-		kfree(idev);
-		return ERR_PTR(-ENOMEM);
-	}
-
-	ion_page_pool = kmem_cache_create("ion_page", PAGE_SIZE, PAGE_SIZE,
-					  SLAB_HWCACHE_ALIGN, NULL);
-	if (!ion_page_pool) {
-		kmem_cache_destroy(ion_sg_table_pool);
-		kfree(idev);
-		return ERR_PTR(-ENOMEM);
-	}
-
 	idev->dev.minor = MISC_DYNAMIC_MINOR;
 	idev->dev.name = "ion";
 	idev->dev.fops = &ion_fops;
@@ -2035,8 +1991,6 @@ struct ion_device *ion_device_create(long (*custom_ioctl)
 	ret = misc_register(&idev->dev);
 	if (ret) {
 		pr_err("ion: failed to register misc device.\n");
-		kmem_cache_destroy(ion_page_pool);
-		kmem_cache_destroy(ion_sg_table_pool);
 		kfree(idev);
 		return ERR_PTR(ret);
 	}
