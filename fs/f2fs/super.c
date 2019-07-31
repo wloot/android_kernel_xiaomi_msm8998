7668916fefc2ad69884f706a2e12ca6beadd7723
Julian Liu <wlootlxt123@gmail.com>
2019-08-01 02:21:05 +0800

    Revert "f2fs: xattr: reserve cache for xattr allocations"
    This reverts commit 0ae7649d599cbe40405c9f539967fa98d65dc6eb.

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -3588,12 +3588,9 @@ static int __init init_f2fs_fs(void)
 	err = init_inodecache();
 	if (err)
 		goto fail;
-	err = f2fs_init_xattr_caches();
-	if (err)
-		goto free_inodecache;
 	err = f2fs_create_node_manager_caches();
 	if (err)
-		goto fail_xattr_caches;
+		goto free_inodecache;
 	err = f2fs_create_segment_manager_caches();
 	if (err)
 		goto free_node_manager_caches;
@@ -3633,8 +3630,6 @@ free_segment_manager_caches:
 	f2fs_destroy_segment_manager_caches();
 free_node_manager_caches:
 	f2fs_destroy_node_manager_caches();
-fail_xattr_caches:
-	f2fs_destroy_xattr_caches();
 free_inodecache:
 	destroy_inodecache();
 fail:
@@ -3652,7 +3647,6 @@ static void __exit exit_f2fs_fs(void)
 	f2fs_destroy_checkpoint_caches();
 	f2fs_destroy_segment_manager_caches();
 	f2fs_destroy_node_manager_caches();
-	f2fs_destroy_xattr_caches();
 	destroy_inodecache();
 	f2fs_destroy_trace_ios();
 }
