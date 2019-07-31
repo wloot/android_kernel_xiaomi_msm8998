7f94de5e1f03a4e517732c0a8d4ef35bb34fb211
celtare21 <celtare21@gmail.com>
2019-07-31 17:07:58 +0000

    sched: deadline: Fix another CAF mismerge of 8623286 and ec888d4
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -1714,7 +1714,6 @@ retry:
 		goto retry;
 	}
 
-	next_task->on_rq = TASK_ON_RQ_MIGRATING;
 	deactivate_task(rq, next_task, 0);
 	clear_average_bw(&next_task->dl, &rq->dl);
 	next_task->on_rq = TASK_ON_RQ_MIGRATING;
@@ -1722,7 +1721,6 @@ retry:
 	next_task->on_rq = TASK_ON_RQ_QUEUED;
 	add_average_bw(&next_task->dl, &later_rq->dl);
 	activate_task(later_rq, next_task, 0);
-	next_task->on_rq = TASK_ON_RQ_QUEUED;
 	ret = 1;
 
 	resched_curr(later_rq);
