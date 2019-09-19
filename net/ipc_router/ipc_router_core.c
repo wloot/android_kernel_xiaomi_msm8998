67a4e2b2b44f9cd53c1eb5bd7a76c885f248d241
Julian Liu <wlootlxt123@gmail.com>
2019-09-20 02:50:14 +0800

    net: ipc_router: Fix memory leak when IPC_LOGGING disabled

--- net/ipc_router/ipc_router_core.c
+++ net/ipc_router/ipc_router_core.c
@@ -3953,6 +3953,7 @@ static void debugfs_init(void)
 static void debugfs_init(void) {}
 #endif
 
+#ifdef CONFIG_IPC_LOGGING
 /**
  * ipc_router_create_log_ctx() - Create and add the log context based on transport
  * @name:	subsystem name
@@ -3972,7 +3973,6 @@ static void *ipc_router_create_log_ctx(char *name)
 	if (!sub_log_ctx)
 		return NULL;
 
-#ifdef CONFIG_IPC_LOGGING
 	sub_log_ctx->log_ctx = ipc_log_context_create(
 				IPC_RTR_INFO_PAGES, name, 0);
 	if (!sub_log_ctx->log_ctx) {
@@ -3981,9 +3981,6 @@ static void *ipc_router_create_log_ctx(char *name)
 		kfree(sub_log_ctx);
 		return NULL;
 	}
-#else
-		return NULL;
-#endif
 
 	strlcpy(sub_log_ctx->log_ctx_name, name,
 			LOG_CTX_NAME_LEN);
@@ -3991,6 +3988,12 @@ static void *ipc_router_create_log_ctx(char *name)
 	list_add_tail(&sub_log_ctx->list, &log_ctx_list);
 	return sub_log_ctx->log_ctx;
 }
+#else
+static void *ipc_router_create_log_ctx(char *name)
+{
+	return NULL;
+}
+#endif
 
 static void ipc_router_log_ctx_init(void)
 {
