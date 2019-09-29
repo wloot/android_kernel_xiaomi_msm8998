78a60f0d7d426a0280880af9efb091b7cc2ee81f
Julian Liu <wlootlxt123@gmail.com>
2019-09-29 22:22:48 +0800

    cpuidle: lpm-levels: Remove unused variable

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1787,7 +1787,6 @@ static int lpm_probe(struct platform_device *pdev)
 {
 	int ret;
 	struct kobject *module_kobj = NULL;
-	struct md_region md_entry;
 
 	get_online_cpus();
 	lpm_root_node = lpm_of_parse_cluster(pdev);
